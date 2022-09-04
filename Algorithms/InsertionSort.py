c8d78d7a668e2147e584f54fca156da23e1633eb
Sanjib Katuwal <katsanjib046@gmail.com>
2022-09-04 15:01:33 -0400

    InsertionSort added

--- Algorithms/InsertionSort.py
+++ Algorithms/InsertionSort.py
@@ -2,6 +2,7 @@
 Contains:
 arraySort
 """
+
 def arraySort(A: list) -> list:
     """Implements  Sort Alogrithm to sort a given array.
     Input:
@@ -17,13 +18,36 @@ def arraySort(A: list) -> list:
             j -= 1
         A[j] = cur
     return A
+
+# import positional list
+import sys
+sys.path.append('../')
+from DataStructures.LinkedLists.doubleLinkedLists import *
+
+
+def positionalListSort(L : PositionalList) -> PositionalList:
+    """Sort PositionalList of comparable elements into non-decreasing order."""
+    if len(L) > 1:
+        marker = L.first()                  # point to the first element
+        while marker !=  L.last():
+            pivot = L.after(marker)         # next item called pivot
+            value = pivot.element()
+            if value > marker.element():    # already sorted
+                marker = pivot              # New marker is the pivot
+            else:
+                walk = marker               # to compare with the lest element
+                while walk != L.first() and L.before(walk).element() > value:
+                    walk = L.before(walk)
+                L.delete(pivot)
+                L.add_before(walk, value)    
             
         
 ######### ----------- Testing -------------- ##################
 if __name__ == "__main__":
-    A = [3,5,6,7,9,2,3]
-    print(arraySort(A))
-    B = []
-    print(arraySort(B))
-    C = [5]
-    print(arraySort(C))
+    pList = PositionalList()
+    pList.add_first(3)
+    pList.add_last(1)
+    pList.add_before(pList.last(), 7)
+    sortedpList = positionalListSort(pList)
+    print(pList.first().element())
+    print(pList.last().element())
