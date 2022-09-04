ff808a42278d098bdc3a1cd450ac2b983d150519
Sanjib Katuwal <katsanjib046@gmail.com>
2022-09-03 23:27:38 -0400

    Implementation of Deque using Doubly  LinkedList Added

--- Data Structures/LinkedLists/doubleLinkedLists.py
+++ Data Structures/LinkedLists/doubleLinkedLists.py
@@ -6,6 +6,11 @@ LinkedDeque
 PositionalList
 """
 
+######### --------- Empty Exception -------------------------##############
+class Empty(Exception):
+    """Raises an error when the lists are empty."""
+    pass
+
 ######### -------- Base for Doubly Linked List -------------- ##############
 
 class _DoublyLinkedBase:
@@ -43,7 +48,7 @@ class _DoublyLinkedBase:
         """Add element e between two existing nodes and return new node."""
         newest = self._Node(e, predecessor, successor)          # Linking it to neighbors
         predecessor._next = newest
-        sucessor._prev = newest                                 # neighbors linked to it
+        successor._prev = newest                                 # neighbors linked to it
         self._size += 1
 
     def _delete_node(self, node):
@@ -59,3 +64,57 @@ class _DoublyLinkedBase:
 
 
 ############# ------------- Deque with a doubly linked list ----------------- ###################
+
+class LinkedDeque(_DoublyLinkedBase):
+    """Double-ended queue implementation based on a doubly linked list. Inhertance of the base class."""
+
+    # Extra __init__ not required
+
+    def first(self):
+        """Returns but doesn't remove the first element."""
+        if self.is_empty():
+            raise Empty("Deque is empty")
+        return self._header._next._element
+
+    def last(self):
+        """Returns but doesn't remove the last item."""
+        if self.is_empty():
+            raise Empty("Deque is empty.")
+        return self._trailer._prev._element
+
+    def insert_first(self, e):
+        """Add an element to the front of the deque."""
+        self._insert_between(e, self._header, self._header._next)
+
+    def insert_last(self, e):
+        """Add an element to the end of the deque."""
+        self._insert_between(e, self._trailer._prev, self._trailer)
+
+    def delete_first(self):
+        """Removes and returns the first element of the deque.
+        Raises an exception if it is empty.
+        """
+        if self.is_empty():
+            raise Empty("Deque is empty.")
+        return self._delete_node(self._header._next)
+
+    def delete_last(self):
+        """Removes and returns the last element of the deque.
+        Raises an exception if it is empty.
+        """
+        if self.is_empty():
+            raise Empty("Deque is empty.")
+        return self._delete_node(self._trailer._prev)
+
+
+############ ----- Testing --------- ##########################
+if __name__ == "__main__":
+    deque = LinkedDeque()
+    print(deque.is_empty())
+    deque.insert_first("a")
+    deque.insert_last("b")
+    print(len(deque))
+    print(deque.delete_first())
+    print(len(deque))
+    print(deque.first())
+
